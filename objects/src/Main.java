e6577a2c1dbc2eaa9a920e1cd9a19e645b2d9abc
ajnoodle <as0023@students.inghamisd.org>
2025-09-05 09:21:26 -0400

    boop

---
+++ objects/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Date;
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//date
+        Date currentDate = new Date();
+        Date anotherDate = new Date();
+        String anotherDateString = anotherDate.toString();
+        anotherDate.setTime(123478);
+        boolean isBefore = currentDate.before(anotherDate);
+        System.out.println(isBefore);
+
+//scanner
+
+        Scanner myScanner = new Scanner(System.in);
+        String resString = myScanner.next();
+        System.out.println(resString + "\n");
+    }
+
+
+
+}
